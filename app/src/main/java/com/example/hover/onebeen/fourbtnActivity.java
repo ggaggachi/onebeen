5ebc15d63e7dd3dab2e1c891ca8d375a9992d288
JinHaengji <wlsgodwl123@naver.com>
2015-08-15 17:11:52 +0900

    행지xml부분

--- app/src/main/java/com/example/hover/onebeen/fourbtnActivity.java
+++ app/src/main/java/com/example/hover/onebeen/fourbtnActivity.java
@@ -2,7 +2,6 @@ package com.example.hover.onebeen;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 

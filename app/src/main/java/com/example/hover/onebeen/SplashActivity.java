026d03c14fce75b70341e474037786ddde20d430
jiniC <csj702@naver.com>
2015-08-26 05:49:09 +0900

    스플래시수정, 페이스북 첫 로그인화면 xml

--- app/src/main/java/com/example/hover/onebeen/SplashActivity.java
+++ app/src/main/java/com/example/hover/onebeen/SplashActivity.java
@@ -8,44 +8,41 @@ package com.example.hover.onebeen;
     import android.content.Intent;
     import android.os.Bundle;
     import android.os.Handler;
+    import android.util.Log;
     import android.view.Window;
     import android.view.WindowManager;
     import com.example.hover.onebeen.puzzle.PuzzleDummyActivity;
 
 public class SplashActivity extends Activity {
 
-        //Set waktu lama splashscreen
-        private static int splashInterval = 2000;
+    private static String TAG = SplashActivity.class.getName();
+    private static long SLEEP_TIME = 3;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            requestWindowFeature(Window.FEATURE_NO_TITLE);
-            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                    WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-            setContentView(R.layout.splashscreen);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
 
-            new Handler().postDelayed(new Runnable() {
-
-
-                @Override
-                public void run() {
-                    // TODO Auto-generated method stub
-                    Intent i = new Intent(SplashActivity.this, MainActivity.class);
-                    startActivity(i);
-
-
-                    //jeda selesai Splashscreen
-                    this.finish();
-                }
-
-                private void finish() {
-                    // TODO Auto-generated method stub
-
-                }
-            }, splashInterval);
-
-        };
+        IntentLauncher launcher = new IntentLauncher();
+        launcher.start();
 
     }
+
+    private class IntentLauncher extends Thread {
+        public void run() {
+
+            try {
+                Thread.sleep(SLEEP_TIME*1000);
+            } catch (Exception e) {
+                Log.e(TAG, e.getMessage());
+            }
+
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            SplashActivity.this.startActivity(intent);
+            SplashActivity.this.finish();
+        }
+    }
+}

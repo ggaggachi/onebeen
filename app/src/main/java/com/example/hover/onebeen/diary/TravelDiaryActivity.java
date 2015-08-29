ea6b7c5fa9389ff9edd7b9200ee21792a717773c
Ssung <ekdxhrl0096@daum.net>
2015-08-29 12:31:48 +0900

    기능 구현 완료

--- app/src/main/java/com/example/hover/onebeen/diary/TravelDiaryActivity.java
+++ app/src/main/java/com/example/hover/onebeen/diary/TravelDiaryActivity.java
@@ -103,18 +103,30 @@ public class TravelDiaryActivity extends AppCompatActivity {
 
                 Log.e("TravelDiaryActivity", "puzzledId:" + puzzleId);
                 Log.e("TravelDiaryActivity", "tag:"+tag);
+                if(TravelStatus.PLANNING.getValue().equals(travelStatus)) {
+                   return;
+                }
 
                 if ("CURRENT".equals(tag)) {
 //                    startActivity(new Intent(TravelDiaryActivity.this, ShowPuzzleActivity.class));
                 } else if ("BEEN".equals(tag)) {
-                    Intent intent = new Intent(TravelDiaryActivity.this, ShowPuzzleActivity.class);
-                    intent.putExtra("id", puzzleId);
-                    startActivity(intent);
+                    if (TravelStatus.ONGOING.getValue().equals(travelStatus)) {
+                        Intent addPuzzleIntent = new Intent(TravelDiaryActivity.this, SavePuzzleActivity.class);
+                        addPuzzleIntent.putExtra("puzzleId", puzzleId);
+//                        addPuzzleIntent.putExtra("travelStatus", travelStatus);
+                        startActivity(addPuzzleIntent);
+                    } else {
+                        Intent intent = new Intent(TravelDiaryActivity.this, ShowPuzzleActivity.class);
+                        intent.putExtra("id", puzzleId);
+                        startActivity(intent);
+                    }
+
                     finish();
                 } else if ("WANT".equals(tag)) {
                     Intent addPuzzleIntent = new Intent(TravelDiaryActivity.this, SavePuzzleActivity.class);
                     addPuzzleIntent.putExtra("puzzleId", puzzleId);
                     startActivity(addPuzzleIntent);
+
                     finish();
                 } else {
                     Intent addPuzzleIntent = new Intent(TravelDiaryActivity.this, AddPuzzleActivity.class);
